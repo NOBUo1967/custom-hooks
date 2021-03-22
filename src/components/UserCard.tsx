cf1dfd4d6f369c90b4a79db10cd5c7e0143f8926
NOBUo <66336936+NOBUo1967@users.noreply.github.com>
2021-03-22 22:26:50 +0900

    [style] カードっぽい外観に変更した。

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -5,10 +5,17 @@ type Props = {
   user: UserProfile;
 };
 
+const style = {
+  border: "solid 1px #ccc",
+  borderRadius: "8px",
+  padding: "0px  16px",
+  margin: "8px"
+};
+
 export const UserCard: VFC<Props> = (props) => {
   const { user } = props;
   return (
-    <div>
+    <div style={style}>
       <dl>
         <dt>名前</dt>
         <dd>{user.name}</dd>
