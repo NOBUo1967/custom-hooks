dc561ba0130016511443ac36c6892ba78821b258
NOBUo <66336936+NOBUo1967@users.noreply.github.com>
2021-03-23 07:05:24 +0900

    [study]カスタムフックの作成 作成自体は簡単 * use〇〇でカスタムフックを作成 * 処理の部分は関数化 * カスタムフックとして提供する部分をreturnで返す。returnはArrayもしくはObjectで返却。関数と一緒に定義したstateを渡す。

---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,40 @@
+// 全ユーザー一覧を取得するカスタムフック
+// onClick押下後に処理する内容
+// ユーザ情報の取得・編集
+// loading・error情報を提供する
+
+import axios from "axios";
+import { useState } from "react";
+import { UserProfile } from "../types/UserProfile";
+import { User } from "../types/api/user";
+
+export const useAllUsers = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getUsers = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name} (${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { getUsers, userProfiles, loading, error };
+};
